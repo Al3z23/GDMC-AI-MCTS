d0a2394613a441ac3a91f6f55cbe2f9d62b5290b
Al3z23 <alejandroguizargarcia@gmail.com>
2025-10-17 14:11:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from gdpc import Editor, Block, Transform, geometry
-from pade.misc.utility import display_message, start_loop
-from pade.core.agent import Agent
-from pade.acl.aid import AID
-from sys import argv
- 
+
 editor = Editor(buffering=True)
  
 # Get a block
